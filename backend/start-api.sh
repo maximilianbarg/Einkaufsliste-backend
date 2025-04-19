db0e9569a3155507305134e60c2306988c81cc46
Maximilian Barg <bargmaxi@gmail.com>
2025-04-19 13:29:06 +0200

    add workers

--- backend/start-api.sh
+++ backend/start-api.sh
@@ -2,8 +2,8 @@
 
 if [ "$DEBUG" = "true" ]; then
   echo "🔧 Starte im Debug-Modus mit Hot Reload..."
-  exec python3 -m debugpy --listen 0.0.0.0:5678 -m fastapi run app/main.py --port 8000 --reload
+  exec python3 -m debugpy --listen 0.0.0.0:5678 -m fastapi run app/main.py --port 8000 --workers 2
 else
   echo "🚀 Starte im Production-Modus..."
-  exec fastapi run app/main.py --port 8000
+  exec fastapi run app/main.py --port 8000 --workers 4
 fi
