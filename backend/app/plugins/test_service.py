ed6c5ac67323fa4d78ae9af231b756df176dda63
Maximilian Barg <bargmaxi@gmail.com>
2025-05-08 22:57:57 +0200

    remove unused

--- backend/app/plugins/test_service.py
+++ backend/app/plugins/test_service.py
@@ -5,10 +5,6 @@ class TestService(BaseService):
     service_name = "Test Service"
 
     async def run(self):
-        print(f"✅ {self.service_name} STARTED")
-        self.logger.info(f"✅ {self.service_name} STARTED")
-        
         while True:
-            self.get_database()
             self.logger.info(f"[{self.service_name}] Doing some background work...")
             await asyncio.sleep(60)
