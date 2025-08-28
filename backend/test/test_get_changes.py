ac4abe13f08c24445504c66560eba22c78c200d0
Maximilian Barg <bargmaxi@gmail.com>
2025-08-28 15:38:42 +0200

    test: add tests for get changes history off

--- backend/test/test_get_changes.py
+++ backend/test/test_get_changes.py
@@ -1,5 +1,5 @@
 import time
-from typing import Dict
+from datetime import datetime, UTC
 
 import pytest
 import requests
@@ -28,9 +28,10 @@ class TestGetChangesAPI(TestBase):
         response = requests.get(f"{url}/collections/{collection_id}/changes", headers=headers)
 
         # then
+        json_response = response.json()
         assert response.status_code == status.HTTP_200_OK
-        assert response.json()["source"] is not None
-        assert response.json()["name"] == f"test_collection"
+        assert json_response["source"] is not None
+        assert json_response["name"] == f"test_collection"
 
         for i, expected_item in enumerate(expected_items):
             self.assert_changes_event(collection_id, "created", i, headers, expected_item)
@@ -64,9 +65,10 @@ class TestGetChangesAPI(TestBase):
         response = requests.get(f"{url}/collections/{collection_id}/changes", headers=headers)
 
         # then
+        json_response = response.json()
         assert response.status_code == status.HTTP_200_OK
-        assert response.json()["source"] is not None
-        assert response.json()["name"] == f"test_collection"
+        assert json_response["source"] is not None
+        assert json_response["name"] == f"test_collection"
 
         for i, expected_item in enumerate(items):
             self.assert_changes_event(collection_id, "created", i, headers, expected_item, filter_string="event=created")
@@ -96,9 +98,10 @@ class TestGetChangesAPI(TestBase):
         response = requests.get(f"{url}/collections/{collection_id}/changes", headers=headers)
 
         # then
+        json_response = response.json()
         assert response.status_code == status.HTTP_200_OK
-        assert response.json()["source"] is not None
-        assert response.json()["name"] == f"test_collection"
+        assert json_response["source"] is not None
+        assert json_response["name"] == f"test_collection"
 
         for i, expected_item in enumerate(items):
             self.assert_changes_event(collection_id, "created", i, headers, expected_item,
@@ -106,3 +109,212 @@ class TestGetChangesAPI(TestBase):
 
         for i, expected_item in enumerate(items):
             self.assert_changes_event(collection_id, "removed", i, headers, expected_item, filter_string="event=removed")
+
+    @pytest.mark.parametrize("items,expected_size", [
+        ([test_item_1], 0),
+    ])
+    def test_get_collection_changes_only_last_modifications_created_edited_removed(self, items, expected_size):
+        # given
+        updated_items = []
+        headers = {"Authorization": f"Bearer {self.access_token}"}
+        response = requests.post(f"{url}/collections/create/test_collection/test", headers=headers)
+
+        collection_id = response.json()["id"]
+
+        for i, item in enumerate(items):
+            # create
+            response = requests.post(f"{url}/collections/{collection_id}/item", headers=headers, json=item)
+
+            item_id = response.json()["id"]
+
+            # edit
+            updated_item_data = item.copy()
+            updated_item_data["name"] = f"new_name_{i}"
+            updated_items.append(updated_item_data)
+
+            requests.put(f"{url}/collections/{collection_id}/item/{item_id}", headers=headers,
+                         json=updated_item_data)
+
+            # remove
+            requests.delete(f"{url}/collections/{collection_id}/item/{item_id}", headers=headers)
+
+        # when
+        response = requests.get(f"{url}/collections/{collection_id}/changes?history=false", headers=headers)
+
+        # then
+        json_response = response.json()
+        assert response.status_code == status.HTTP_200_OK
+        assert json_response["source"] is not None
+        assert json_response["name"] == f"test_collection"
+        assert len(json_response["data"]) == expected_size
+
+
+
+    @pytest.mark.parametrize("items,expected_size", [
+        ([test_item_1], 2),
+    ])
+    def test_get_collection_changes_only_last_modifications_created_edited_edited(self, items, expected_size):
+        # given
+        updated_items = []
+        headers = {"Authorization": f"Bearer {self.access_token}"}
+        response = requests.post(f"{url}/collections/create/test_collection/test", headers=headers)
+
+        collection_id = response.json()["id"]
+
+        for i, item in enumerate(items):
+            # create
+            response = requests.post(f"{url}/collections/{collection_id}/item", headers=headers, json=item)
+
+            item_id = response.json()["id"]
+
+            # edit
+            updated_item_data = item.copy()
+            updated_item_data["name"] = f"new_name_{i}"
+
+            requests.put(f"{url}/collections/{collection_id}/item/{item_id}", headers=headers,
+                         json=updated_item_data)
+
+            # edit
+            updated_item_data_2 = item.copy()
+            updated_item_data_2["name"] = f"new_name_{i*10}"
+            updated_items.append(updated_item_data_2)
+
+            requests.put(f"{url}/collections/{collection_id}/item/{item_id}", headers=headers,
+                         json=updated_item_data_2)
+
+        # when
+        response = requests.get(f"{url}/collections/{collection_id}/changes?history=false", headers=headers)
+
+        # then
+        json_response = response.json()
+        assert response.status_code == status.HTTP_200_OK
+        assert json_response["source"] is not None
+        assert json_response["name"] == f"test_collection"
+        assert len(json_response["data"]) == expected_size
+
+        for i, expected_item in enumerate(items):
+            self.assert_changes_event(collection_id, "created", i, headers, expected_item,
+                                      filter_string="event=created")
+
+        for i, expected_item in enumerate(updated_items):
+            self.assert_changes_event(collection_id, "edited", i, headers, expected_item,
+                                      filter_string="event=edited")
+
+    @pytest.mark.parametrize("items,expected_size", [
+        ([test_item_1], 1),
+    ])
+    def test_get_collection_changes_only_last_modifications_created(self, items, expected_size):
+        # given
+        headers = {"Authorization": f"Bearer {self.access_token}"}
+        response = requests.post(f"{url}/collections/create/test_collection/test", headers=headers)
+
+        collection_id = response.json()["id"]
+
+        for i, item in enumerate(items):
+            # create
+            requests.post(f"{url}/collections/{collection_id}/item", headers=headers, json=item)
+
+        # when
+        response = requests.get(f"{url}/collections/{collection_id}/changes?history=false", headers=headers)
+
+        # then
+        json_response = response.json()
+        assert response.status_code == status.HTTP_200_OK
+        assert json_response["source"] is not None
+        assert json_response["name"] == f"test_collection"
+        assert len(json_response["data"]) == expected_size
+
+        for i, expected_item in enumerate(items):
+            self.assert_changes_event(collection_id, "created", i, headers, expected_item,
+                                      filter_string="event=created")
+
+    @pytest.mark.parametrize("items,expected_size", [
+        ([test_item_1], 1),
+    ])
+    def test_get_collection_changes_only_last_modifications_edited(self, items, expected_size):
+        # given
+        updated_items = []
+        headers = {"Authorization": f"Bearer {self.access_token}"}
+        response = requests.post(f"{url}/collections/create/test_collection/test", headers=headers)
+
+        collection_id = response.json()["id"]
+
+        timestamp = ""
+
+        for i, item in enumerate(items):
+            # create
+            response = requests.post(f"{url}/collections/{collection_id}/item", headers=headers, json=item)
+
+            item_id = response.json()["id"]
+
+            response = requests.get(f"{url}/collections/{collection_id}/info", headers=headers)
+            timestamp = response.json()["data"]["last_modified"]
+
+            # edit
+            updated_item_data = item.copy()
+            updated_item_data["name"] = f"new_name_{i}"
+            updated_items.append(updated_item_data)
+
+            requests.put(f"{url}/collections/{collection_id}/item/{item_id}", headers=headers,
+                         json=updated_item_data)
+
+        # when
+        response = requests.get(f"{url}/collections/{collection_id}/changes?filter=timestamp>{timestamp}&history=false", headers=headers)
+
+        # then
+        json_response = response.json()
+        assert response.status_code == status.HTTP_200_OK
+        assert json_response["source"] is not None
+        assert json_response["name"] == f"test_collection"
+        assert len(json_response["data"]) == expected_size
+
+        for i, expected_item in enumerate(updated_items):
+            self.assert_changes_event(collection_id, "edited", i, headers, expected_item,
+                                      filter_string="event=edited")
+
+    @pytest.mark.parametrize("items,expected_size", [
+        ([test_item_1], 1),
+    ])
+    def test_get_collection_changes_only_last_modifications_edited_removed(self, items, expected_size):
+        # given
+        updated_items = []
+        headers = {"Authorization": f"Bearer {self.access_token}"}
+        response = requests.post(f"{url}/collections/create/test_collection/test", headers=headers)
+
+        collection_id = response.json()["id"]
+
+        timestamp = ""
+
+        for i, item in enumerate(items):
+            # create
+            response = requests.post(f"{url}/collections/{collection_id}/item", headers=headers, json=item)
+
+            item_id = response.json()["id"]
+
+            response = requests.get(f"{url}/collections/{collection_id}/info", headers=headers)
+            timestamp = response.json()["data"]["last_modified"]
+
+            # edit
+            updated_item_data = item.copy()
+            updated_item_data["name"] = f"new_name_{i}"
+            updated_items.append(updated_item_data)
+
+            requests.put(f"{url}/collections/{collection_id}/item/{item_id}", headers=headers,
+                         json=updated_item_data)
+
+            # remove
+            requests.delete(f"{url}/collections/{collection_id}/item/{item_id}", headers=headers)
+
+        # when
+        response = requests.get(f"{url}/collections/{collection_id}/changes?filter=timestamp>{timestamp}&history=false", headers=headers)
+
+        # then
+        json_response = response.json()
+        assert response.status_code == status.HTTP_200_OK
+        assert json_response["source"] is not None
+        assert json_response["name"] == f"test_collection"
+        assert len(json_response["data"]) == expected_size
+
+        for i, expected_item in enumerate(updated_items):
+            self.assert_changes_event(collection_id, "removed", i, headers, expected_item,
+                                      filter_string="event=removed")
